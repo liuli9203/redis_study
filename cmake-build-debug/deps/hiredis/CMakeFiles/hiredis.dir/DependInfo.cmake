292653ee7b48ca52e154223d1a7849ce0f921395
carpenter <liuli9203@gmail.com>
2019-03-07 13:45:18 +0800

    'cmakelists.txt'

---
+++ cmake-build-debug/deps/hiredis/CMakeFiles/hiredis.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "D:/Projects/CLion/redis_study/deps/hiredis/async.c" "D:/Projects/CLion/redis_study/cmake-build-debug/deps/hiredis/CMakeFiles/hiredis.dir/async.c.obj"
+  "D:/Projects/CLion/redis_study/deps/hiredis/dict.c" "D:/Projects/CLion/redis_study/cmake-build-debug/deps/hiredis/CMakeFiles/hiredis.dir/dict.c.obj"
+  "D:/Projects/CLion/redis_study/deps/hiredis/hiredis.c" "D:/Projects/CLion/redis_study/cmake-build-debug/deps/hiredis/CMakeFiles/hiredis.dir/hiredis.c.obj"
+  "D:/Projects/CLion/redis_study/deps/hiredis/net.c" "D:/Projects/CLion/redis_study/cmake-build-debug/deps/hiredis/CMakeFiles/hiredis.dir/net.c.obj"
+  "D:/Projects/CLion/redis_study/deps/hiredis/read.c" "D:/Projects/CLion/redis_study/cmake-build-debug/deps/hiredis/CMakeFiles/hiredis.dir/read.c.obj"
+  "D:/Projects/CLion/redis_study/deps/hiredis/sds.c" "D:/Projects/CLion/redis_study/cmake-build-debug/deps/hiredis/CMakeFiles/hiredis.dir/sds.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/deps/hiredis/CMakeFiles/hiredis.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hiredis.dir/hiredis.c.obj"
+  "CMakeFiles/hiredis.dir/net.c.obj"
+  "CMakeFiles/hiredis.dir/dict.c.obj"
+  "CMakeFiles/hiredis.dir/sds.c.obj"
+  "CMakeFiles/hiredis.dir/async.c.obj"
+  "CMakeFiles/hiredis.dir/read.c.obj"
+  "libhiredis.pdb"
+  "libhiredis.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/hiredis.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
